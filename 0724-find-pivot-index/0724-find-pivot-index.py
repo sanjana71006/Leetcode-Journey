cd008de39866a5a4f7d3ce06b5c3fc9bbefa19ea
sanjana71006 <sanjanapriyadarshini6@gmail.com>
2026-07-29 20:53:31 +0530

    Time: 8 ms (38.63%), Space: 20.7 MB (6.09%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.py
+++ 0724-find-pivot-index/0724-find-pivot-index.py
@@ -1,14 +1,14 @@
 class Solution:
     def pivotIndex(self, nums: List[int]) -> int:
-        n=len(nums)
         ps=nums.copy()
+        n=len(nums)
         for i in range(1,n):
-            ps[i]=ps[i]+ps[i-1]
-        ss=nums.copy()
+            ps[i]=ps[i-1]+ps[i]
+        ss=[0]*n
+        ss[-1]=nums[-1]
         for i in range(n-2,-1,-1):
-            ss[i]=ss[i]+ss[i+1]
-        for i in range(0,n):
+            ss[i]=nums[i]+ss[i+1]
+        for i in range(n):
             if ss[i]==ps[i]:
-                return i 
+                return i
         return -1
-        
